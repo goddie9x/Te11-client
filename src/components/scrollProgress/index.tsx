5e617f9a92438e478d60a8ace9b4712354b4cb88
Hoang Tam <goddie9x@yahoo.com>
2022-01-09 15:08:55 +0700

    fix: hide scroll progress if client view in top

--- src/components/scrollProgress/index.tsx
+++ src/components/scrollProgress/index.tsx
@@ -7,12 +7,16 @@ const TScrollProgress = (props: TLinearProgressProps) => {
   const [progress, setProgress] = useState(0);
 
   const handleScroll = () => {
-    const scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || window.scrollTo(0, 0) || 0;
-    const scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight  || 0;
+    const scrollTop =
+      window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || window.scrollTo(0, 0) || 0;
+    const scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight || 0;
     const clientHeight = document.documentElement.clientHeight || document.body.clientHeight || 0;
     const scrolled = Math.floor((scrollTop / (scrollHeight - clientHeight)) * 100);
-    
-    setProgress(scrolled);
+    if (scrolled > 2) {
+      setProgress(scrolled);
+    } else {
+      setProgress(0);
+    }
   };
 
   useEffect(() => {
@@ -22,7 +26,7 @@ const TScrollProgress = (props: TLinearProgressProps) => {
       window.removeEventListener('scroll', handleScroll);
     };
   });
-  return <TLinearProgress value={progress} {...props} />;
+  return progress ? <TLinearProgress value={progress} {...props} /> : null;
 };
 
 export default TScrollProgress;

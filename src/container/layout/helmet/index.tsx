793980830d5fc064ca9fedb4a398da700aa536bf
Hoang Tam <goddie9x@yahoo.com>
2022-01-11 17:08:51 +0700

    feat: add Helmet for SEO

---
+++ src/container/layout/helmet/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import {Helmet} from "react-helmet";
+
+const THelmet = ()=>{
+    //TODO: will change it after get API
+    const title = "Te11 app";
+    const relativePath = "https://te11test.herokuapp.com";
+    const description = "Te11 app description";
+    const verification = "https://te11test.herokuapp.com";
+
+    return <Helmet>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" /> 
+        <meta name="description" content={description} />
+        <meta name="google-site-verification" content={verification}/>
+        <link rel="canonical" href={relativePath}/>
+    </Helmet>;
+};
+
+export default THelmet;
--- src/container/layout/index.tsx
+++ src/container/layout/index.tsx
@@ -2,6 +2,7 @@ import TBox from 'components/box';
 import React from 'react';
 import TFooter from './footer';
 import THeader from './header';
+import THelmet from './helmet';
 
 export type TLayoutProps = {
   children: React.ReactNode;
@@ -10,8 +11,9 @@ export type TLayoutProps = {
 const TLayout = ({ children }: TLayoutProps) => {
   return (
     <>
+      <THelmet />
       <THeader />
-      <TBox margin="80px 0 30px 0">{children}</TBox>
+      <TBox margin="80px 30px 30px 30px">{children}</TBox>
       <TFooter />
     </>
   );
